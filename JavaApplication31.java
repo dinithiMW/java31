4fb058a339b43c27a5e1c89158c5c92485b4e521
[dinithi] <[dinithimweerasinghe95@gmail.com>
2019-03-03 18:45:31 +0530

    java31
    java31

---
+++ JavaApplication31.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication31;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication31 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String myString="Hello Youtube";
+        int myStringlength=myString.length();
+        String myStringInCase=myString.toLowerCase();
+        
+        System.out.println(myString);
+        System.out.println(myStringlength);
+        System.out.println(myStringInCase);
+    }
+    
+}
